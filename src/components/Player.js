9313d5f4188a02846fa678a02db8b8abc653eb08
codetann <thetannerthomas@gmail.com>
2020-11-17 14:26:54 -0700

    add preventdefault

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,9 +3,10 @@ import { Form, FormControl, Button } from "react-bootstrap";
 
 export default function Player({ round, score, setScore, players, data }) {
   const input1 = useRef(null);
-  const handleScore = () => {
+  const handleScore = (e) => {
     if (isNaN(parseInt(input1.current.value))) {
     } else {
+      e.preventDefault();
       players[data.id].score =
         parseInt(score[data.id].score) + parseInt(input1.current.value);
       setScore(players);
@@ -19,7 +20,7 @@ export default function Player({ round, score, setScore, players, data }) {
         <h4 className="score">Score: {data.score}</h4>
       </div>
 
-      <Form className="form">
+      <Form className="form" onSubmit={handleScore}>
         <FormControl ref={input1} disabled={round <= 0 ? true : false} />
         <Button onClick={handleScore} disabled={round <= 0 ? true : false}>
           +
